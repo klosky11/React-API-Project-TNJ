5e67e00cc6e5187cf31f9e461f0c741da9ab4193
IamCoy <jacoyiaj@yahoo.com>
2022-02-11 15:57:51 -0500

    working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+import "./header.css"
+
+
 export function Header() {
-  return <div></div>;
+
+    return (
+      <div>
+        <header>
+          {/* just some placeholder text and background for now */}
+            <h1>Plan your getaway and your play</h1>
+            
+        </header>
+
+        <nav>
+        <Link to= "/home">Home</Link>
+        <Link to= "/saved">Saved Trips</Link>
+        <Link to= "/spontenous">Feeling Spontenous?</Link>
+        
+        </nav>
+
+        </div>
+    );
 }
