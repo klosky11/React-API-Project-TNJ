a61a91d373ab3279834254c8dbfa50282915890e
IamCoy <jacoyiaj@yahoo.com>
2022-02-21 01:15:33 -0500

    "merged"

--- src/components/WeatherResult.tsx
+++ src/components/WeatherResult.tsx
@@ -3,21 +3,7 @@ import "../css/weatherResults.css";
 import { propTypes } from "react-bootstrap/esm/Image";
 
 export function WeatherResult(prop: { weather?: any[] }) {
-  function mapWeatherList() {
-    prop.weather?.map((item) => {
-      return (
-        <div>
-          <li key={item.datetime} className="weatherCell">
-            {" "}
-            <WeatherResultListForecasted
-              weather={item}
-            ></WeatherResultListForecasted>{" "}
-          </li>
-          <hr></hr>
-        </div>
-      );
-    });
-  }
+ 
   return (
     <div>
       <h1 className="weather-header"> Weather during your stay :</h1>
